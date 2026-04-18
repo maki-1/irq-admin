fbb51a6b63259d95324ae9249b831c6c53f5693e
Marpygit commit -m Initial <marpy112@gmail.com>
2026-04-19 01:48:02 +0800

    Add Backend Service

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL || ''}/api` });
 
 // Attach JWT to every request
 api.interceptors.request.use((config) => {
